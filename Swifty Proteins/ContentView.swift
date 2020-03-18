f1f874c1af423517af337e80786a3e03946b3504
Clément P <cpieri@student.42.fr>
2020-03-18 10:53:57 +0100

    Initial Commit

---
+++ Swifty Proteins/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swifty Proteins
+//
+//  Created by Louise on 18/03/2020.
+//  Copyright © 2020 Louise Pieri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
